77d9ef6939c3f8177013e55124001a44897fcacd
Luiz Henrique Barbosa <luizhenriquebarbosa21@gmail.com>
2024-08-29 23:12:03 -0300

    feat: Desafio Concluído

--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia");
+Nokia telefone1 = new Nokia ("981430583", "AX3002", "123456", 16);
+telefone1.PropiedadesSmartphone();
+telefone1.Ligar();
+telefone1.ReceberLigacao();
+telefone1.InstalarAplicativo("Facebook");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone");
+Iphone telefone2 = new Iphone("981763432", "2024XD", "987654", 128);
+telefone2.PropiedadesSmartphone();
+telefone2.Ligar();
+telefone2.ReceberLigacao();
+telefone2.InstalarAplicativo("DU Recorder");
